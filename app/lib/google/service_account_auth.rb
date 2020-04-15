7e7f80ef76c8984ae7c61981133c0fbae9b72b2a
Dan <d.kachur@datarockets.com>
2020-04-15 14:48:00 +0300

    Remove ENV vars hack

--- app/lib/google/service_account_auth.rb
+++ app/lib/google/service_account_auth.rb
@@ -14,35 +14,10 @@ module Google
       @g_scopes ||= scopes.map { |scope| G_SCOPE[scope] }
       @logger ||= logger
 
-      set_required_env_vars
-
-      @credentials = Google::Auth.get_application_default(@g_scopes)
+      @credentials = Google::Auth::DefaultCredentials.make_creds(scope: @g_scopes, json_key_io: StringIO.new(@config.to_json, 'r'))
       @credentials.fetch_access_token!
 
-      cleanup!
-
       @logger.info("Initialize #{self.class.name}")
-    end
-
-    private
-
-    def set_required_env_vars
-      if @config['key'].present?
-        # https://github.com/google/google-auth-library-ruby/issues/65
-        # https://github.com/google/google-api-ruby-client/issues/370
-        ENV['GOOGLE_PRIVATE_KEY'] = @config['key']
-        ENV['GOOGLE_CLIENT_EMAIL'] = @config['service_account_email']
-        ENV['GOOGLE_ACCOUNT_TYPE'] = 'service_account'
-      elsif @config['key_file'].present?
-        ENV['GOOGLE_APPLICATION_CREDENTIALS'] = @config['key_file']
-      end
-    end
-
-    def cleanup!
-      ENV.delete('GOOGLE_PRIVATE_KEY')
-      ENV.delete('GOOGLE_CLIENT_EMAIL')
-      ENV.delete('GOOGLE_ACCOUNT_TYPE')
-      ENV.delete('GOOGLE_APPLICATION_CREDENTIALS')
     end
   end
 end
